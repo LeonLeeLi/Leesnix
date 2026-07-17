d4dc8b009f10bfd0e3f8ddad5ec4dffb5eda87f4
Leon Lee <leonleeli@outlook.com>
2026-07-17 18:20:29 +0800

    fix source add

--- flake.nix
+++ flake.nix
@@ -17,6 +17,10 @@
       url = "github:leonleeli/LeesNixPackage";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs =
     {
