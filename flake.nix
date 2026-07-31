c25f937737b91124afc5b988c14ffd388f8221af
Leon Lee <leonleeli@outlook.com>
2026-07-31 19:33:58 +0800

    change cursor

--- flake.nix
+++ flake.nix
@@ -37,6 +37,10 @@
       url = "github:nix-community/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    noctalia-greeter = {
+      url = "github:noctalia-dev/noctalia-greeter";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs =
     {
--- modules/desktop/niri.nix
+++ modules/desktop/niri.nix
@@ -1,8 +1,20 @@
-{ ... }:
+{ pkgs, inputs, ... }:
 {
+  imports = [
+    inputs.noctalia-greeter.nixosModules.default
+  ];
   programs.niri.enable = true;
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
-  # noctalia 在 home 层 (home/desktop/niri.nix) 启用,避免 theme.source
-  # 在 NixOS 层和 home 层重复定义导致冲突。
+
+  stylix = {
+    enable = true;
+    polarity = "dark";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Classic";
+      size = 24;
+    };
+  };
 }
